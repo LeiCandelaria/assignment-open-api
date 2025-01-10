588d6a0dd4d03b0e5061a6c947fd01670970987c
LeiCandelaria <leilaanne.candelaria@cvsu.edu.ph>
2025-01-11 00:20:27 +0800

    updated appmodule again

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,13 @@
 import { Module,MiddlewareConsumer } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { OcrService } from './ocr/ocr.service';
 import { OcrController } from './ocr/ocr.controller';
 import { AuthMiddleware } from './auth/auth.middleware';
 import { OcrModule } from './ocr/ocr.module';
-import { OcrModule } from './ocr/ocr.module';
-import { OcrModule } from './ocr/ocr.module';
 
 @Module({
-  imports: [AppModule, OcrModule],
-  controllers: [AppController, OcrController],
-  providers: [AppService, OcrService],
+  imports: [],
 })
 export class AppModule {
-  configure(consumer:MiddlewareConsumer){
-    consumer.apply(AuthMiddleware).forRoutes('ocr'); // Applying Middleware to ocr routes//
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('*'); //Applying the middleware to all routes//
   }
 }
