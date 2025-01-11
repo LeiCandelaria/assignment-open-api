86d280bafe5eba5dd0ee8fa6e05c9535e9d5cfd8
LeiCandelaria <leilaanne.candelaria@cvsu.edu.ph>
2025-01-11 15:02:31 +0800

    controller updated

--- src/ocr/ocr.controller.ts
+++ src/ocr/ocr.controller.ts
@@ -1,38 +1,22 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors,Body} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import {Multer} from 'multer';
-import * as fs from 'fs';
 import { OcrService } from './ocr.service';
+import {Multer} from 'multer'
+
 
 @Controller('ocr')
 export class OcrController {
   constructor(private readonly ocrService: OcrService) {}
 
-  @Post('extractTextFromImage')
-  @UseInterceptors(
-    FileInterceptor('file', {   
-      storage: Multer.diskStorage({
-        destination: './uploads', // Directory to save uploaded files//
-        filename: (req, file, cb) => {
-          const uniqueName = `${Date.now()}-${file.originalname}`;
-          cb(null, uniqueName);
-        },
-      }),
-    }),
-  )
-  async handleFileUpload(@UploadedFile() file: Multer.File): Promise<{ text: string }> {
-    if (!file) {
-      throw new Error('No file uploaded');
-    }
-
-    try {
-      const text = await this.ocrService.extractTextFromImage(file.path);
-      return { text };
-    } catch (error) {
-      throw new Error(`OCR processing failed: ${error.message}`);
-    } finally {
-      fs.unlinkSync(file.path); // Clean up uploaded file
-    }
+  @Post('img')
+  async extractFromImage(@Body('imageUrl') imageUrl: string) {
+    return await this.ocrService.extractTextFromImage(imageUrl);
   }
 
+  @Post('pdf')
+  @UseInterceptors(FileInterceptor('file'))
+  async extractFromPdf(@UploadedFile() file:Multer.File) {
+    return await this.ocrService.extractTextFromPdf(file.path);
+  }
 }
+
