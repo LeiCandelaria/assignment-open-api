bdc20b0ced8f1248394ed76e36571fb6fe840250
LeiCandelaria <leilaanne.candelaria@cvsu.edu.ph>
2025-01-11 18:01:06 +0800

    main.ts port update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
+  const uploadDir = path.join(__dirname, '..', 'uploads');
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 9898);
+  app.enableCors(); // Enable CORS for cross-origin requests
+  await app.listen(8989);
 }
 bootstrap();
